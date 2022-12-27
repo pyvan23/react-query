abd8e6881d3f179ebea73973777c92fc518d02e7
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-12-27 19:54:18 -0300

    return products

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -4,7 +4,7 @@ const productsApi = axios.create({
   baseURL: "http://localhost:3000",
 });
 
-const getProducts = async () => {
-    const res =   await productsApi.get("/products");
+export const getProducts = async () => {
+    const resp =   await productsApi.get("/products");
     return resp.data
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
 
+import { getProducts } from '../api/productsApi'
 
 export const Products = () => {
 
+    const { isLoading, data:products, isError , error} = useQuery({
+        queryKey:['products'],
+        queryFn: getProducts
+
+    })
+
+    if(isLoading) return <>...Loading</>
+    if(isError) return <>{error.message}</>
+
+    console.log(products)
+    return products.map(product=>
+        <>
+        <div key={product.id}>{product.name}</div>
+        </>)
     
-
-
-  return (
-    <div>Products</div>
-  )
+        
+    
 }
