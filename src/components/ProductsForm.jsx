047e5ea365a3c912eb7fa8e35c9c901966018baf
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-12-31 17:43:50 -0300

    usequery invalid queries

--- src/components/ProductsForm.jsx
+++ src/components/ProductsForm.jsx
@@ -1,20 +1,32 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { postProducts } from "../api/productsApi";
 
-export const ProductsForm = () => {
-  const addProductMutation = useMutation({ mutationFn: postProducts });
 
-  const handleSubmit = ( e ) => {
+export const ProductsForm = () => {
+  
+  const queryClient = useQueryClient();
+
+  const addProductMutation = useMutation({
+    mutationFn: postProducts,
+     onSuccess: () => {
+      queryClient.invalidateQueries('products')
+      console.log('el producto se agrego')
+    }
+  });
+  
+
+  const handleSubmit = (e) => {
 
     e.preventDefault();
-    const formData = new FormData( e.target );
-    const products = Object.fromEntries( formData );
-    addProductMutation.mutate({ ...products, stock:true   });
+    const formData = new FormData(e.target);
+    const products = Object.fromEntries(formData);
+    addProductMutation.mutate({ ...products, stock: true });
+  
 
   };
 
   return (
-    <form onSubmit={ handleSubmit } action="text">
+    <form onSubmit={handleSubmit} action="text">
       <label htmlFor="name">name</label>
       <input type="text" id="name" name="name" />
       <label htmlFor="description">Description</label>
