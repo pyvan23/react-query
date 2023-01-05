62bd97603b75b1bcfa4eef0ef5c0e716a6fa3650
Ivan <85912778+pyvan23@users.noreply.github.com>
2023-01-05 14:10:44 +0000

    query cache implemenyts to search

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import "./App.css";
 import { Products } from "./components/Products";
 import { ProductsForm } from "./components/ProductsForm";
 import { Queries } from "./components/Queries";
+import { QueryCache } from 'react-query';
+
 
 function App() {
   return (
     <>
+     <QueryCache>
       <Queries/>
       <ProductsForm/>
       <Products />
+    </QueryCache>
     </>
   );
 }
